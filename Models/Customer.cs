1ad71234099d680583c50f4599699431f1cb32c0
Stephanie Kasunick <sk611715@ohio.edu>
2020-01-29 11:43:26 -0500

    1/29/2020

---
+++ Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sk611715MIS4200.Models
+{
+    public class Customer
+    {
+        public int customerID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime customerSince { get; set; }
+
+        //connect the tables
+        public ICollection<Orders> Orders { get; set; }
+    }
+}
---
+++ Models/Orders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sk611715MIS4200.Models
+{
+    public class Orders
+    {
+        [Key]
+        public int orderNum { get; set; }
+        public string description { get; set; }
+        public DateTime orderDate { get; set; }
+        public int customerID { get; set; }
+
+        //connect the tables
+        public virtual Customer Customer { get; set; }
+    }
+}
