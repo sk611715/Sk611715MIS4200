c038b51fa45ab3ed0213280a8f482719b2216105
Stephanie Kasunick <sk611715@ohio.edu>
2020-02-23 21:42:57 -0500

    SD3

--- Models/Album.cs
+++ Models/Album.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,21 @@ namespace Sk611715MIS4200.Models
     public class Album
     {
         public int albumID { get; set; }
+
+        [Display(Name = "Album Name")]
+        [Required(ErrorMessage = "Album name is required")]
+        [StringLength(50)]
         public string albumName { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required(ErrorMessage = "Genre is required")]
+        [StringLength(50)]
         public string genre { get; set; }
+
+        [Display(Name = "Release Date")]
+        [Required(ErrorMessage = "Release Date is required")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:d}", ApplyFormatInEditMode = true)]
         public DateTime releaseDate { get; set; }
 
         //Connect to Artist Album
--- Models/Artist.cs
+++ Models/Artist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,15 @@ namespace Sk611715MIS4200.Models
     public class Artist
     {
         public int artistID { get; set; }
+
+        [Display (Name="Artist Name")]
+        [Required(ErrorMessage = "Artist name is required")]
+        [StringLength(50)]
         public string artistName { get; set; }
+
+        [Display (Name = "Age")]
+        [Required(ErrorMessage = "Artist Age is required")]
+        [Range(0,150)]
         public int age { get; set; }
 
         //connect to ArtistAlbum
